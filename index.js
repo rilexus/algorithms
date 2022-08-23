ca17c228cc7d43f12d7d9a9657877e33fb77521e
rilexus <stanip@hotmail.de>
2022-08-23 18:03:35 +0200

    feat: linked list

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const linearSearch = require("./src/linearSearch");
 const binarySearch = require("./src/binarySearch");
 const bubbleSort = require("./src/bubbleSort");
+const LinkedList = require("./src/LinkedList");
 
-console.log(bubbleSort([6, 5, 4, 3, 2, 1]));
+const list = new LinkedList();
+
+list.append(1);
+list.append(4);
+list.prepend(2);
+list.prepend(1);
+list.append(5);
+
+console.log(list.toString());
---
+++ src/LinkedList.js
@@ -0,0 +1,58 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    if (this.head === null) {
+      // empty list
+      this.head = { value, next: null };
+      this.tail = this.head;
+    } else {
+      this.tail.next = { value, next: null };
+      this.tail = this.tail.next;
+    }
+  }
+
+  prepend(value) {
+    if (this.head === null) {
+      // empty list
+      this.head = { value, next: null };
+      this.tail = this.head;
+    } else {
+      this.head = { value, next: this.head };
+    }
+  }
+
+  toArray() {
+    let el = this.head;
+    const res = [];
+    do {
+      res.push(el.value);
+      el = el.next;
+    } while (el !== null);
+
+    return res;
+  }
+
+  toString() {
+    return [...this].join(", ");
+  }
+
+  [Symbol.iterator]() {
+    let el = this.head;
+    return {
+      next() {
+        if (el === null) {
+          return { done: true, value: null };
+        }
+        const res = { done: false, value: el.value };
+        el = el.next;
+        return res;
+      },
+    };
+  }
+}
+
+module.exports = LinkedList;
